8cd7da0534fe36e5873429276a02789e5a33503e
Brian Duong <brianminhduong@gmail.com>
2022-07-15 21:03:54 -0400

    Bind handlers for form/submit in SimpleInput component

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,9 +1,21 @@
+import { useRef, useState } from "react";
+
 const SimpleInput = (props) => {
+  const [enteredName, setEnteredName] = useState("");
+
+  const nameInputChangeHandler = (e) => {
+    setEnteredName(e.target.value);
+  };
+
+  const formSubmissionHandler = (e) => {
+    e.preventDefault();
+  };
+
   return (
-    <form>
-      <div className='form-control'>
-        <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+    <form onSubmit={formSubmissionHandler}>
+      <div className="form-control">
+        <label htmlFor="name">Your Name</label>
+        <input type="text" id="name" onChange={nameInputChangeHandler} />
       </div>
       <div className="form-actions">
         <button>Submit</button>
