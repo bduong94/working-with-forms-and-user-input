6e9c3ffdab60bdf4f6718c88daa24d5b74f6acc8
Brian Duong <brianminhduong@gmail.com>
2022-07-15 21:08:50 -0400

    Add reset to input after submission

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,6 +1,7 @@
 import { useRef, useState } from "react";
 
 const SimpleInput = (props) => {
+  const nameInputRef = useRef();
   const [enteredName, setEnteredName] = useState("");
 
   const nameInputChangeHandler = (e) => {
@@ -9,13 +10,25 @@ const SimpleInput = (props) => {
 
   const formSubmissionHandler = (e) => {
     e.preventDefault();
+
+    console.log(enteredName);
+    const enteredValue = nameInputRef.current.value;
+    console.log(enteredValue);
+
+    setEnteredName("");
   };
 
   return (
     <form onSubmit={formSubmissionHandler}>
       <div className="form-control">
         <label htmlFor="name">Your Name</label>
-        <input type="text" id="name" onChange={nameInputChangeHandler} />
+        <input
+          ref={nameInputRef}
+          type="text"
+          id="name"
+          onChange={nameInputChangeHandler}
+          value={enteredName}
+        />
       </div>
       <div className="form-actions">
         <button>Submit</button>
