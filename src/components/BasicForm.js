0c4d5ca9dd0e7de5b1b86c8585f9f00d68461777
Brian Duong <brianminhduong@gmail.com>
2022-07-18 21:19:06 -0400

    Implement useInput hook with first name input

--- src/components/BasicForm.js
+++ src/components/BasicForm.js
@@ -19,16 +19,35 @@ const BasicForm = (props) => {
     reset: firstNameInputReset,
   } = useInput((value) => value.trim() !== "");
 
+  const firstNameClass = firstNameHasError
+    ? "form-control invalid"
+    : "form-control";
+
   const onSubmitHandler = (e) => {
     e.preventDefault();
+
+    if (!enteredFirstNameIsValid) {
+      return;
+    }
+
+    firstNameInputReset();
   };
 
   return (
     <form onSubmit={onSubmitHandler}>
       <div className="control-group">
-        <div className="form-control">
+        <div className={firstNameClass}>
           <label htmlFor="name">First Name</label>
-          <input type="text" id="name" />
+          <input
+            type="text"
+            id="name"
+            onChange={firstNameChangeHandler}
+            onBlur={firstNameBlurHandler}
+            value={enteredFirstName}
+          />
+          {firstNameHasError && (
+            <p className="error-text">First Name must not be empty!</p>
+          )}
         </div>
         <div className="form-control">
           <label htmlFor="name">Last Name</label>
