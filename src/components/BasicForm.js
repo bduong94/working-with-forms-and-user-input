727dde3a7633efc61b7095bc6c3cfd30093b35f2
Brian Duong <brianminhduong@gmail.com>
2022-07-18 21:13:02 -0400

    Add states for first name input for Basic Form

--- src/components/BasicForm.js
+++ src/components/BasicForm.js
@@ -1,21 +1,45 @@
+import useInput from "../hooks/use-input";
+
 const BasicForm = (props) => {
+  // return {
+  //   value: enteredValue,
+  //   isValid: valueIsValid,
+  //   hasError,
+  //   valueChangeHandler,
+  //   inputBlurHandler,
+  //   reset,
+  // };
+
+  const {
+    value: enteredFirstName,
+    isValid: enteredFirstNameIsValid,
+    hasError: firstNameHasError,
+    valueChangeHandler: firstNameChangeHandler,
+    inputBlurHandler: firstNameBlurHandler,
+    reset: firstNameInputReset,
+  } = useInput((value) => value.trim() !== "");
+
+  const onSubmitHandler = (e) => {
+    e.preventDefault();
+  };
+
   return (
-    <form>
-      <div className='control-group'>
-        <div className='form-control'>
-          <label htmlFor='name'>First Name</label>
-          <input type='text' id='name' />
+    <form onSubmit={onSubmitHandler}>
+      <div className="control-group">
+        <div className="form-control">
+          <label htmlFor="name">First Name</label>
+          <input type="text" id="name" />
         </div>
-        <div className='form-control'>
-          <label htmlFor='name'>Last Name</label>
-          <input type='text' id='name' />
+        <div className="form-control">
+          <label htmlFor="name">Last Name</label>
+          <input type="text" id="name" />
         </div>
       </div>
-      <div className='form-control'>
-        <label htmlFor='name'>E-Mail Address</label>
-        <input type='text' id='name' />
+      <div className="form-control">
+        <label htmlFor="name">E-Mail Address</label>
+        <input type="text" id="name" />
       </div>
-      <div className='form-actions'>
+      <div className="form-actions">
         <button>Submit</button>
       </div>
     </form>
